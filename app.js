db79b1a51eb572c6e7593e46445e34a9ebede84a
taromaster <161418482+jungjongkyu@users.noreply.github.com>
2024-02-27 15:14:09 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 document.getElementById('drawCardBtn').addEventListener('click', drawCard);
 
 function drawCard() {
-    const cards = [
+    const cardNames = [
         "0번 바보",
         "1번 마법사",
         "2번 여사제",
@@ -26,13 +26,13 @@ function drawCard() {
         "21번 월드",
     ];
 
-    const selectedCardName = cards[Math.floor(Math.random() * cards.length)];
-    const encodedCardName = encodeURI(selectedCardName); // 파일 이름을 URL 인코딩
-    const imageUrl = `https://taromaster.github.io/${encodedCardName}.jpg`; // 인코딩된 파일 이름을 포함하는 이미지 URL
+    const selectedCardName = cardNames[Math.floor(Math.random() * cardNames.length)];
+    const encodedCardName = encodeURI(selectedCardName); // 인코딩하여 URL에 포함될 수 있도록 처리
+    const imageUrl = `https://jungjongkuy.github.io/repository/${taromaster.github.io}.jpg`; // 귀하의 GitHub 사용자 이름과 저장소 이름으로 바꾸세요
     displayCard(imageUrl);
 }
 
 function displayCard(imageUrl) {
     const resultDiv = document.getElementById('cardResult');
-    resultDiv.innerHTML = `<img src="${imageUrl}" alt="타로 카드" class="w-40 h-60 mx-auto">`; // 선택된 카드를 표시
+    resultDiv.innerHTML = `<img src="${imageUrl}" alt="타로 카드" class="w-40 h-60 mx-auto">`;
 }
